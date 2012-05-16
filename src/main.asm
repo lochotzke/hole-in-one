5d6c3aca29414c0c0069c3e27d279f6a85cb3629
André Lochotzke <andre@lochotzke.name>
2012-05-16 11:11:26 +0200

    finally got it working

--- src/main.asm
+++ src/main.asm
@@ -1,6 +1,9 @@
+; http://pic-projects.net/
 ; TODO: Fehlerbetrachtung, statische Analyse (Zeitaufwand, Codezeilen, Speicherverbrauch, ...)
 ; TODO: document calculations (round and countdown)
 ; TODO: determine T_RELEASE through trial and error
+
+; picp -c /dev/ttyUSB0 16f84a -ef && picp -c /dev/ttyUSB0 16f84a -wp *.hex
 
 ; 2012 © André Lochotzke <andre.lochotzke@stud.fh-erfurt.de>
 
@@ -21,9 +24,11 @@
 #define		MAGNET_PORT	PORTB
 #define		MAGNET_PIN	2
 
-#define		T_COMPUTE	79
+;#define		T_COMPUTE	79
+#define		T_COMPUTE	0
 #define 	T_RELEASE	0
 #define		T_DROP		316121
+;#define		T_DROP		0
 #define		TMR_Z		0
 
 		udata
@@ -34,7 +39,6 @@ TMR1		res		1
 TMR2		res		1
 t_round		res		3
 t_slot		res		3
-;n		res		1
 		errorlevel	+231	; "No memory has been reserved by this instruction."
 
 start		code		0
@@ -44,7 +48,6 @@ irq		code		4
 		goto		isr
 
 isr
-; http://www.pic-projects.net/index.php?option=com_content&view=article&id=53:multi-byte-increment&catid=38:arithmetic&Itemid=57
 		incf		TMR1, F
 		btfsc		STATUS, Z
 		incf		TMR2, F
@@ -76,6 +79,16 @@ init
 		; set all to one
 		movlw		H'FF'
 		movwf		PORTB
+		; ram needs to be cleared since it can contain garbage
+		clrf		FLAGS
+		clrf		TMR1
+		clrf		TMR2
+		clrf		t_round
+		clrf		t_round + 1
+		clrf		t_round + 2
+		clrf		t_slot
+		clrf		t_slot + 1
+		clrf		t_slot + 2
 		; enable interrupts
 		bsf		INTCON, GIE
 		goto		main
@@ -90,7 +103,7 @@ main_loop_trigger_start
 		goto		main_loop_trigger_start
 main_loop_trigger_stop
 ; DEBUG
-		bsf		PORTB, 1
+;		bsf		PORTB, 1
 		bcf		PORTB, 3
 		
 		; poll trigger release
@@ -98,11 +111,12 @@ main_loop_trigger_stop
 		goto		main_loop_trigger_stop
 
 ; DEBUG
-		bsf		PORTB, 3
+;		bsf		PORTB, 3
 		bcf		PORTB, 4
 
 		; poll slot start
 main_loop_slot_start
+; TODO: maybe use btfsc
 		btfss		SENSOR_PORT, SENSOR_PIN		; 2µs (start slot)
 		goto		main_loop_slot_start
 
@@ -114,16 +128,17 @@ main_loop_slot_start
 		bsf		INTCON, T0IE			; (1µs)
 
 ; DEBUG
-		bsf		PORTB, 4
+;		bsf		PORTB, 4
 		bcf		PORTB, 5
 
 		; poll slot end
 main_loop_slot_end
+; TODO: maybe use btfss
 		btfsc		SENSOR_PORT, SENSOR_PIN		; 2µs (end slot)
 		goto		main_loop_slot_end
 
 ; DEBUG
-		bsf		PORTB, 5
+;		bsf		PORTB, 5
 		bcf		PORTB, 6
 
 		; stop timer
@@ -154,18 +169,6 @@ main_loop_slot_end
 		rlf		TMR1, F
 		rlf		TMR2, F
 		; TMR += t_slot
-; movf    A0,W
-;         addwf   R0,F
-;         movf    A1,W
-;         btfsc   STATUS,C
-;         incfsz  A1,W
-;         addwf   R1,F
-;         movf    A2,W
-;         btfsc   STATUS,C
-;         incfsz  A2,W
-;         addwf   R2,F
-; TODO: another math fix?
-; http://www.pic-projects.net/index.php?option=com_content&view=article&id=57:multi-byte-addition-a-subtraction&catid=38:arithmetic&Itemid=57
 		movf		t_slot, W			; 10µs
 		addwf		TMR0, F
 		movf		t_slot + 1, W
@@ -197,17 +200,6 @@ main_loop_slot_end
 		movlw		(T_COMPUTE + T_RELEASE + T_DROP) >> 16 & H'ff'
 		movwf		TMR2
 
-; movf    A0,W
-;         addwf   R0,F
-;         movf    A1,W
-;         btfsc   STATUS,C
-;         incfsz  A1,W
-;         addwf   R1,F
-;         movf    A2,W
-;         btfsc   STATUS,C
-;         incfsz  A2,W
-;         addwf   R2,F
-
 ; TODO: another math fix?
 		; add t_slot to countdown
 		movf		t_slot, W			; 10µs
@@ -221,16 +213,6 @@ main_loop_slot_end
 		incfsz		t_slot + 2, W
 		addwf		TMR2, F
 
-;         movf    A0,W
-;         subwf   R0,F
-;         movf    A1,W
-;         btfss   STATUS,C
-;         incfsz  A1,W
-;         subwf   R1,F
-;         movf    A2,W
-;         btfss   STATUS,C
-;         incfsz  A2,W
-;         subwf   R2,F
 main_loop_calc
 		; adjust countdown to cause drop in current or next round
 		movf		t_round, W			; min. 12µs max. 25µs
@@ -244,48 +226,13 @@ main_loop_calc
 		incfsz		t_round + 2, W
 		subwf		TMR2, F
 ; TODO: maybe btfsc is the fix?
+; http://picprojects.org.uk/projects/pictips.htm
+; FIXME: too many rounds
 		btfsc		STATUS, C
 		goto		main_loop_calc
 
-; 		clrf		n
-; ;FIXME: count instructions
-; main_loop_count
-; 		incf		n
-; 		; adjust countdown to cause drop in current or next round
-; 		movf		t_round, W			; min. 12µs max. 25µs
-; 		subwf		TMR0, F
-; 		movf		t_round + 1, W
-; 		btfss		STATUS, C
-; 		incfsz		t_round + 1, W
-; 		subwf		TMR1, F
-; 		movf		t_round + 2, W
-; 		btfss		STATUS, C
-; 		incfsz		t_round + 2, W
-; 		subwf		TMR2, F
-; 		btfss		STATUS, C
-; 		goto		main_loop_count
-; 
-; main_loop_adjust
-; 		; adjust countdown to cause drop in current or next round
-; 		decf		n
-; 		btfsc		STATUS, Z
-; 		goto		main_loop_adjust_done
-; 		movf		t_round, W			; min. 12µs max. 25µs
-; 		subwf		TMR0, F
-; 		movf		t_round + 1, W
-; 		btfss		STATUS, C
-; 		incfsz		t_round + 1, W
-; 		subwf		TMR1, F
-; 		movf		t_round + 2, W
-; 		btfss		STATUS, C
-; 		incfsz		t_round + 2, W
-; 		subwf		TMR2, F
-; 		btfss		STATUS, C
-; 		goto		main_loop_adjust
-; main_loop_adjust_done
-
 ; DEBUG
-		bsf		PORTB, 6
+;		bsf		PORTB, 6
 		bcf		PORTB, 7
 
 		; start timer/countdown
@@ -302,7 +249,9 @@ main_loop_countdown
 		; drop ball
 		bcf		MAGNET_PORT, MAGNET_PIN
 
-		; re-run
-		goto		init
+;		bsf		PORTB, 7
+
+		; halt
+		goto		$
 
 		end
